6ba5c7980c45b24ceeb41d67f618cbd9dbd33207
shaipranesh2 <f20200731@pilani.bits-pilani.ac.in>
2023-05-05 23:42:13 +0300

    deployed website

--- src/Nav.js
+++ src/Nav.js
@@ -5,13 +5,13 @@ export default function Nav(){
         <div className="nav-bar">
             <div className="Name"><p><strong>Shai Pranesh</strong></p></div>
             <div className="Home"><a href="">Home</a></div>
-            <div className="Blog"><a href="https://shaipranesh2.github.io/blog.github.io/">Blog</a></div>
+            <div className="Blog"><a href="https://shaipranesh2.github.io/blog.github.io/" target="_blank">Blog</a></div>
             <div className="Experience"><a href="">Experience</a></div>
             <div className="Projects"><a href="">Projects</a></div>
             <div className="Courses"><a href="">Courses</a></div>
             <div className="Skills"><a href="">Skills</a></div>
             
-            <div className="Resume"><a href="https://drive.google.com/drive/u/1/folders/1aM5lNWdUU80g7ZlJutdmcZAdVx0ASt7V">Resume</a></div>
+            <div className="Resume"><a href="https://drive.google.com/drive/u/1/folders/1aM5lNWdUU80g7ZlJutdmcZAdVx0ASt7V" target="_blank">Resume</a></div>
             <div className="Contact"><a href="">Contact</a></div>
             
         </div>
