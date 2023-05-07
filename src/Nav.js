fe625a1b3ed2f1e50bbf56839996b3de61ba459f
shaipranesh2 <f20200731@pilani.bits-pilani.ac.in>
2023-05-07 08:30:01 +0300

    deployed website

--- src/Nav.js
+++ src/Nav.js
@@ -2,18 +2,22 @@ import React from "react";
 
 export default function Nav(){
     return(
-        <div className="nav-bar">
+        <nav>
             <div className="Name"><p><strong>Shai Pranesh</strong></p></div>
-            <div className="Home"><a href="">Home</a></div>
-            <div className="Blog"><a href="https://shaipranesh2.github.io/blog.github.io/" target="_blank">Blog</a></div>
-            <div className="Experience"><a href="">Experience</a></div>
-            <div className="Projects"><a href="">Projects</a></div>
-            <div className="Courses"><a href="">Courses</a></div>
-            <div className="Skills"><a href="">Skills</a></div>
+        <div className="nav-elements">
+        <ul>
             
-            <div className="Resume"><a href="https://drive.google.com/drive/u/1/folders/1aM5lNWdUU80g7ZlJutdmcZAdVx0ASt7V" target="_blank">Resume</a></div>
-            <div className="Contact"><a href="">Contact</a></div>
+            <li><div className="Home"><a href="">Home</a></div></li>
+            <li><div className="Blog"><a href="https://shaipranesh2.github.io/blog.github.io/" target="_blank">Blog</a></div></li>
+            <li><div className="Experience"><a href="">Experience</a></div></li>
+            <li><div className="Projects"><a href="">Projects</a></div></li>
+            <li><div className="Courses"><a href="">Courses</a></div></li>
+            <li><div className="Skills"><a href="">Skills</a></div></li>
             
+            <li><div className="Resume"><a href="https://drive.google.com/drive/u/1/folders/1aM5lNWdUU80g7ZlJutdmcZAdVx0ASt7V" target="_blank">Resume</a></div></li>
+            <li><div className="Contact"><a href="">Contact</a></div></li>
+        </ul>
         </div>
+        </nav>
     )
 }
